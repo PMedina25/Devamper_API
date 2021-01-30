d0c3205f4c4350696f4516b7587c624b403bf3bc
pmedina <1amedinapablo@gmail.com>
2021-01-30 18:40:46 +0100

    Lot of changes for serverless

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
+const serverless = require('serverless-http');
 const app = require('./app');
 const port = require('./config/keys').PORT;
 const NODE_ENV = require('./config/keys').NODE_ENV;
 
-
 const PORT = port || 5000;
 
-const server = app.listen(PORT, console.log(`Server running in ${NODE_ENV} mode on port ${PORT}`.yellow.bold));
+// const server = app.listen(PORT, console.log(`Server running in ${NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
@@ -13,3 +13,5 @@ process.on('unhandledRejection', (err, promise) => {
     // Close server & exit process
     server.close(() => process.exit(1));
 })
+
+module.exports = serverless(app);
